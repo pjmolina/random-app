809502e23c83063b80f14371db64a77523ad86ea
Pedro J. Molina <pjmolina@gmail.com>
2023-06-14 17:01:27 +0200

    fix random generator. fix #28

--- public/random.js
+++ public/random.js
@@ -6,20 +6,20 @@ function clearAll() {
     renderNumber('-');
 }
 
-function generateRandomNumber() {
-    return 9;
-}
-
 // function generateRandomNumber() {
-//     return Math.floor(Math.random() * 10);
+//     return 9;
 // }
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 10);
+}
 
 function renderNumber(text) {
     document.getElementById("generado").innerHTML = text;
 }
 
 function getAppVersion() {
-    document.getElementById("appVersion").innerHTML = "1.3.0";
+    document.getElementById("appVersion").innerHTML = "1.3.1";
 }
 
 function init() {
